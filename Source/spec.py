5f35f345d1fd86ebfd40cb243539206cae9b1c2d
Ongsalt <ongsa.srp.mkm@gmail.com>
2024-11-04 14:19:32 +0700

    add class spec

---
+++ Source/spec.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+
+def is_collided(self, player_hitbox: pygame.Rect) -> bool: ...
+
+# velocity = 10px / sec
+# velocity = 10/60 px / frame
+# รันทุกเฟรม ต้องขยับวัตถุ
+def update(self): ...
+def draw(self, screen: pygame.Surface): ...
+
+# nine | 8 Nov
+class Obstacle:
+    pass
+
+# - 
+class ScorePoint:
+    pass
+
+# one | 6 Nov
+class ColorChanger:
+    pass
+
+class Background:
+    pass
+class Block:
+    pass
+
+
+# Ongsa 
+# game logic
+class Scene:
+    pass
+class Player:
+    pass
