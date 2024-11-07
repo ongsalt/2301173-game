45c51f08b140378fdfb2c1b83308a4c1a28bffed
Ongsalt <ongsa.srp.mkm@gmail.com>
2024-11-07 13:49:53 +0700

    add tinting utils

---
+++ Source/utils.py
@@ -0,0 +1,10 @@
+import pygame
+
+def tint(surf, tint_color):
+    """ adds tint_color onto surf.
+    """
+    surf = surf.copy()
+    surf.fill((0, 0, 0, 255), None, pygame.BLEND_RGBA_MULT)
+    # surf.fill(tint_color[0:3] + (0,), None, pygame.BLEND_RGBA_ADD)
+    return surf
+
