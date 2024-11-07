747189d3b74d059c0b0757a0cbad6d071a0545d8
Hazwan67 <6734483523@student.chula.ac.th>
2024-11-07 16:30:11 +0700

    add copy,draw method

--- Source/color_change.py
+++ Source/color_change.py
@@ -2,17 +2,35 @@ import pygame
 from pygame.locals import *
 
 class ColorChange(pygame.sprite.Sprite): 
-    def __init__(self,x,y,color):
+    def __init__(self,x,y,color,image):
         super().__init__()
         self.color = color
+        self.image = image
         self.x = x
         self.y = y
-        self.image = pygame.Surface((30, 600))
-        self.image.fill(self.color)
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.rect(screen,self.color,self.rect)
+    def copy(self):
+        return ColorChange(self.rect.x,self.rect.y,self.color,self.image)
     def update(self):
         self.rect.x -= 10
     def is_collided(self, player_hitbox: pygame.Rect) -> bool:
         return self.rect.colliderect(player_hitbox)
+
+if __name__ == "__main__":
+    pygame.init()
+    clock = pygame.time.Clock()
+    DISPLAY_SIZE = (800, 600)
+
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        # เทสตรงนี้นะครับ
+
+        pygame.display.flip()
