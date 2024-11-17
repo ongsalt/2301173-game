c59a579a0f2fe4f7bd47afa0efe054c9638b4fac
Ongsalt <ongsa.srp.mkm@gmail.com>
2024-11-18 00:59:02 +0700

    feat: ui: add key binding hint

--- kaoyum/ui/overlay/home.py
+++ kaoyum/ui/overlay/home.py
@@ -7,30 +7,75 @@ from ..animation import Loop, Spring, Acceleration
 class HomeUI(Widget):
     def __init__(self, size):
         self.size = size
+        self.static_surface = Surface(size, SRCALPHA, 32)
         self.surface = Surface(size, SRCALPHA, 32)
-        self.dirty = True
         self.offset_x = Acceleration(0, acceleration=0, max_speed=750)
         self.title_text = Text("Game Title", "upheavtt.ttf", 50, (255, 255, 255))
         self.description_text = Text("Press Space to start", "upheavtt.ttf", 20, (255, 255, 255))
         self.description_text_opacity = Loop(160, 235, angular_frequency=.5)
+        self.static_part_opacity = Spring(255, natural_freq=10)
+        self.prerender()
+
+    def prerender(self):
+        # draw key binding at the bottom
+        y = self.size[1] - 16 - 24
+        x = 12
+
+        w_key = AssetsManager().get("keys/w.png", (32, 32))
+        self.static_surface.blit(w_key, (x, y))
+        up_key = AssetsManager().get("keys/up.png", (32, 32))
+        self.static_surface.blit(up_key, (x + 32, y))
+        text = Text("Move Up", "upheavtt.ttf", 20, (255, 255, 255))
+        text.draw(self.static_surface, (x + 68, y + 10))
+
+        x = text.size[0] + 92 + 24
+
+        s_key = AssetsManager().get("keys/s.png", (32, 32))
+        self.static_surface.blit(s_key, (x, y))
+        down_key = AssetsManager().get("keys/down.png", (32, 32))
+        self.static_surface.blit(down_key, (x + 32, y))
+        text = Text("Move Down", "upheavtt.ttf", 20, (255, 255, 255))
+        text.draw(self.static_surface, (x + 68, y + 10))
+
+        x = self.size[0] - 12
+        text = Text("Start / Restart", "upheavtt.ttf", 20, (255, 255, 255))
+        x -= text.size[0]
+        text.draw(self.static_surface, (x, y + 10))
+        x -= 72
+        space_key = AssetsManager().get("keys/space.png", (64, 32))
+        self.static_surface.blit(space_key, (x, y))
+
+        text = Text("Pause", "upheavtt.ttf", 20, (255, 255, 255))
+        x -= text.size[0] + 24
+        text.draw(self.static_surface, (x, y + 10))
+        x -= 60
+        esc_key = AssetsManager().get("keys/esc.png", (64, 32))
+        self.static_surface.blit(esc_key, (x, y))
+        
 
     def draw(self, display: Surface, offset: tuple[int, int] = (0, 0)):
-        self.description_text.color = (255, 255, 255, self.description_text_opacity.value)
-        self.surface.fill((0, 0, 0, 0))
         w, h = self.size
-        x1 = self.offset_x.value + (w // 2 - self.title_text.size[0] // 2)
-        x2 = self.offset_x.value + (w // 2 - self.description_text.size[0] // 2)
+        self.surface.fill((0, 0, 0, 0), (0, 0, w, 400))
+        x1 =(w // 2 - self.title_text.size[0] // 2)
         self.title_text.draw(self.surface, (x1, 120))
+
+        w, h = self.size
+        self.description_text.color = (255, 255, 255, self.description_text_opacity.value)
+        x2 =(w // 2 - self.description_text.size[0] // 2)
         self.description_text.draw(self.surface, (x2, 180))
-        self.dirty = False
-        display.blit(self.surface, offset)
-    
-    def select(self):
-        pass
+        x, y = offset
+        x += self.offset_x.value
+
+        self.static_surface.set_alpha(self.static_part_opacity.value)
+        display.blit(self.static_surface, offset)
+        display.blit(self.surface, (x, y))
 
     def update(self, dt = 1000 / 60, is_game_started = False):
         self.description_text_opacity.update(dt)
+        self.static_part_opacity.update(dt)
         self.offset_x.update(dt)
+
         if is_game_started:
             if not self.offset_x.is_animating:
                 self.offset_x.animate_to(-1000, initial_velocity=400)
+                self.static_part_opacity.animate_to(0)
--- kaoyum/ui/overlay/pause_menu.py
+++ kaoyum/ui/overlay/pause_menu.py
@@ -1,24 +1,44 @@
 from pygame import SRCALPHA
+import pygame
 from pygame.surface import Surface
 from kaoyum.ui.animation import Spring
-from ..widget import Widget
+from ..widget import Widget, Text
 
 class PauseMenu(Widget):
     def __init__(self, size: tuple[int, int]):
         self.surface = Surface(size, SRCALPHA, 32)
         self.hidden = True
-        self.scale = Spring(1, natural_freq=5)
+        self.opacity = Spring(0)
+        self.size = size
+        self.scale = Spring(.85)
+        self.text = Text("Mai Tam Tee", "upheavtt.ttf", 60, (255, 255, 255))
 
     def update(self, dt: float = 1000 / 60):
-        pass
+        self.scale.update(dt)
+        self.opacity.update(dt)
 
     def draw(self, display: Surface, offset: tuple[int, int] = (0, 0)):
-        pass
+        # if self.hidden:
+        #     return
+
+        w, h = self.size
+        self.surface.fill((0, 0, 0, 0))
+        x = w // 2 - self.text.size[0] // 2
+        self.text.draw(self.surface, (x, h // 2 - self.text.size[1] // 2))
+        self.surface.set_alpha(self.opacity.value)
+
+        scaled = pygame.transform.scale(self.surface, (int(w * self.scale.value), int(h * self.scale.value)))
+        x, y = offset
+        x += (w - scaled.get_width()) // 2
+        y += (h - scaled.get_height()) // 2
+        display.blit(scaled, (x, y))
 
     def show(self):
         self.hidden = False
         self.scale.animate_to(1)
+        self.opacity.animate_to(255)
 
     def hide(self):
         self.hidden = True
-        self.scale.animate_to(0)
+        self.scale.animate_to(.85)
+        self.opacity.animate_to(0)
